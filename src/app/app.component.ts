546c8e38786e25915aff0315ea5851b08f80332f
ngarcia11 <49655787+ngarcia11@users.noreply.github.com>
2019-08-13 10:21:27 -0600

    Cambios nuevo formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   users = ['Sady', 'Keyla',  'Juan'];
+  activate= false;
 
+  nombre: string = 'Sady Garcia';
+   edad: number = 21;
+  name: string = 'Norlan Garcia' ;
+ age: number = 21;
+ address: {
+street: string;
+city: string ;
+};
+hobbies: string[];
+
+constructor(){
+  this.name = 'Norlan Garcia';
+  this.age = 21;
+   this.address = {
+  street : 'Calle del comercio',
+  city : 'Tegucigalpa'
+ };
+this.hobbies = ['Nadar', 'Leer', 'Programar'];
+}
+
+delete(user) {
+for(let i=0; i < this.users.length; i ++){
+  if (user == this.users[i]){
+    this.users.splice(i,1);
+  }
+}
+}
+
+addUser(newUser){
+this.users.push(newUser.value);
+newUser.value = "";
+newUser.focus;
+return false;
+}
 }
